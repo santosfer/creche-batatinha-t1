6bebe4595bdb4f4167f5201f7019c566aae3399e
pausa109 <palmeida.dev@gmail.com>
2023-04-29 11:59:22 -0300

    Atualizacao de nome de variavel

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,105 +1,105 @@
-function Avaliar(patinha) {
+function Pontuar(patinha) {
     var url = window.location;
     url = url.toString()
     url = url.split("cadastro.html");
     url = url[0];
    
-    var s1 = document.getElementById("s1").src;
-    var s2 = document.getElementById("s2").src;
-    var s3 = document.getElementById("s3").src;
-    var s4 = document.getElementById("s4").src;
-    var s5 = document.getElementById("s5").src;
-    var avaliacao = 0;
+    var p1 = document.getElementById("p1").src;
+    var p2 = document.getElementById("p2").src;
+    var p3 = document.getElementById("p3").src;
+    var p4 = document.getElementById("p4").src;
+    var p5 = document.getElementById("p5").src;
+    var pontuacao = 0;
    
    if (patinha == 5){ 
-    if (s5 == url + "imagens/patavazia.png") {
-    document.getElementById("s1").src = "imagens/patacheia.png";
-    document.getElementById("s2").src = "imagens/patacheia.png";
-    document.getElementById("s3").src = "imagens/patacheia.png";
-    document.getElementById("s4").src = "imagens/patacheia.png";
-    document.getElementById("s5").src = "imagens/patacheia.png";
-    avaliacao = 5;
+    if (p5 == url + "imagens/patavazia.png") {
+    document.getElementById("p1").src = "imagens/patacheia.png";
+    document.getElementById("p2").src = "imagens/patacheia.png";
+    document.getElementById("p3").src = "imagens/patacheia.png";
+    document.getElementById("p4").src = "imagens/patacheia.png";
+    document.getElementById("p5").src = "imagens/patacheia.png";
+    pontuacao = 5;
     } else {
-    document.getElementById("s1").src = "imagens/patacheia.png";
-    document.getElementById("s2").src = "imagens/patacheia.png";
-    document.getElementById("s3").src = "imagens/patacheia.png";
-    document.getElementById("s4").src = "imagens/patacheia.png";
-    document.getElementById("s5").src = "imagens/patavazia.png";
-    avaliacao = 4;
+    document.getElementById("p1").src = "imagens/patacheia.png";
+    document.getElementById("p2").src = "imagens/patacheia.png";
+    document.getElementById("p3").src = "imagens/patacheia.png";
+    document.getElementById("p4").src = "imagens/patacheia.png";
+    document.getElementById("p5").src = "imagens/patavazia.png";
+    pontuacao = 4;
    }}
     
     //patinha 4
    if (patinha == 4){ 
-    if (s4 == url + "imagens/patavazia.png") {
-    document.getElementById("s1").src = "imagens/patacheia.png";
-    document.getElementById("s2").src = "imagens/patacheia.png";
-    document.getElementById("s3").src = "imagens/patacheia.png";
-    document.getElementById("s4").src = "imagens/patacheia.png";
-    document.getElementById("s5").src = "imagens/patavazia.png";
-    avaliacao = 4;
+    if (p4 == url + "imagens/patavazia.png") {
+    document.getElementById("p1").src = "imagens/patacheia.png";
+    document.getElementById("p2").src = "imagens/patacheia.png";
+    document.getElementById("p3").src = "imagens/patacheia.png";
+    document.getElementById("p4").src = "imagens/patacheia.png";
+    document.getElementById("p5").src = "imagens/patavazia.png";
+    pontuacao = 4;
     } else {
-    document.getElementById("s1").src = "imagens/patacheia.png";
-    document.getElementById("s2").src = "imagens/patacheia.png";
-    document.getElementById("s3").src = "imagens/patacheia.png";
-    document.getElementById("s4").src = "imagens/patavazia.png";
-    document.getElementById("s5").src = "imagens/patavazia.png";
-    avaliacao = 3;
+    document.getElementById("p1").src = "imagens/patacheia.png";
+    document.getElementById("p2").src = "imagens/patacheia.png";
+    document.getElementById("p3").src = "imagens/patacheia.png";
+    document.getElementById("p4").src = "imagens/patavazia.png";
+    document.getElementById("p5").src = "imagens/patavazia.png";
+    pontuacao = 3;
    }}
    
    //patinha 3
    if (patinha == 3){ 
-    if (s3 == url + "imagens/patavazia.png") {
-    document.getElementById("s1").src = "imagens/patacheia.png";
-    document.getElementById("s2").src = "imagens/patacheia.png";
-    document.getElementById("s3").src = "imagens/patacheia.png";
-    document.getElementById("s4").src = "imagens/patavazia.png";
-    document.getElementById("s5").src = "imagens/patavazia.png";
-    avaliacao = 3;
+    if (p3 == url + "imagens/patavazia.png") {
+    document.getElementById("p1").src = "imagens/patacheia.png";
+    document.getElementById("p2").src = "imagens/patacheia.png";
+    document.getElementById("p3").src = "imagens/patacheia.png";
+    document.getElementById("p4").src = "imagens/patavazia.png";
+    document.getElementById("p5").src = "imagens/patavazia.png";
+    pontuacao = 3;
     } else {
-    document.getElementById("s1").src = "imagens/patacheia.png";
-    document.getElementById("s2").src = "imagens/patacheia.png";
-    document.getElementById("s3").src = "imagens/patavazia.png";
-    document.getElementById("s4").src = "imagens/patavazia.png";
-    document.getElementById("s5").src = "imagens/patavazia.png";
-    avaliacao = 2;
+    document.getElementById("p1").src = "imagens/patacheia.png";
+    document.getElementById("p2").src = "imagens/patacheia.png";
+    document.getElementById("p3").src = "imagens/patavazia.png";
+    document.getElementById("p4").src = "imagens/patavazia.png";
+    document.getElementById("p5").src = "imagens/patavazia.png";
+    pontuacao = 2;
    }}
     
    //patinha 2
    if (patinha == 2){ 
-    if (s2 == url + "imagens/patavazia.png") {
-    document.getElementById("s1").src = "imagens/patacheia.png";
-    document.getElementById("s2").src = "imagens/patacheia.png";
-    document.getElementById("s3").src = "imagens/patavazia.png";
-    document.getElementById("s4").src = "imagens/patavazia.png";
-    document.getElementById("s5").src = "imagens/patavazia.png";
-    avaliacao = 2;
+    if (p2 == url + "imagens/patavazia.png") {
+    document.getElementById("p1").src = "imagens/patacheia.png";
+    document.getElementById("p2").src = "imagens/patacheia.png";
+    document.getElementById("p3").src = "imagens/patavazia.png";
+    document.getElementById("p4").src = "imagens/patavazia.png";
+    document.getElementById("p5").src = "imagens/patavazia.png";
+    pontuacao = 2;
     } else {
-    document.getElementById("s1").src = "imagens/patacheia.png";
-    document.getElementById("s2").src = "imagens/patavazia.png";
-    document.getElementById("s3").src = "imagens/patavazia.png";
-    document.getElementById("s4").src = "imagens/patavazia.png";
-    document.getElementById("s5").src = "imagens/patavazia.png";
-    avaliacao = 1;
+    document.getElementById("p1").src = "imagens/patacheia.png";
+    document.getElementById("p2").src = "imagens/patavazia.png";
+    document.getElementById("p3").src = "imagens/patavazia.png";
+    document.getElementById("p4").src = "imagens/patavazia.png";
+    document.getElementById("p5").src = "imagens/patavazia.png";
+    pontuacao = 1;
    }}
     
     //patinha 1
    if (patinha == 1){ 
-    if (s1 == url + "imagens/patavazia.png") {
-    document.getElementById("s1").src = "imagens/patacheia.png";
-    document.getElementById("s2").src = "imagens/patavazia.png";
-    document.getElementById("s3").src = "imagens/patavazia.png";
-    document.getElementById("s4").src = "imagens/patavazia.png";
-    document.getElementById("s5").src = "imagens/patavazia.png";
-    avaliacao = 1;
+    if (p1 == url + "imagens/patavazia.png") {
+    document.getElementById("p1").src = "imagens/patacheia.png";
+    document.getElementById("p2").src = "imagens/patavazia.png";
+    document.getElementById("p3").src = "imagens/patavazia.png";
+    document.getElementById("p4").src = "imagens/patavazia.png";
+    document.getElementById("p5").src = "imagens/patavazia.png";
+    pontuacao = 1;
     } else {
-    document.getElementById("s1").src = "imagens/patavazia.png";
-    document.getElementById("s2").src = "imagens/patavazia.png";
-    document.getElementById("s3").src = "imagens/patavazia.png";
-    document.getElementById("s4").src = "imagens/patavazia.png";
-    document.getElementById("s5").src = "imagens/patavazia.png";
-    avaliacao = 0;
+    document.getElementById("p1").src = "imagens/patavazia.png";
+    document.getElementById("p2").src = "imagens/patavazia.png";
+    document.getElementById("p3").src = "imagens/patavazia.png";
+    document.getElementById("p4").src = "imagens/patavazia.png";
+    document.getElementById("p5").src = "imagens/patavazia.png";
+    pontuacao = 0;
    }}
     
-    document.getElementById('rating').innerHTML = avaliacao;
+    document.getElementById('avaliacao').innerHTML = pontuacao;
     
    }
