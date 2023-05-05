017ddbff05b1bf243d5e6c54e038d827ec9aba1a
Fernanda Santos <santo4fer@gmail.com>
2023-05-04 21:10:59 -0300

    quebrando a cabeça

---
+++ scripts/avaliar.js
@@ -0,0 +1,9 @@
+// const clicouBtn = document.getElementById('btn1')
+//    .addEventListener('click', lancarNota)
+
+
+// function lancarNota() {
+//    window.location.href = `“./avaliacao.html”`
+
+// }
+
