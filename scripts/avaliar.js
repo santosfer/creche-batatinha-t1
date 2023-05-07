8689f051c62a3ad96497fcd7ad2a6b529004d487
Fernanda Santos <santo4fer@gmail.com>
2023-05-07 17:39:42 -0300

    att document

--- scripts/avaliar.js
+++
@@ -1,9 +0,0 @@
-// const clicouBtn = document.getElementById('btn1')
-//    .addEventListener('click', lancarNota)
-
-
-// function lancarNota() {
-//    window.location.href = `“./avaliacao.html”`
-
-// }
-
